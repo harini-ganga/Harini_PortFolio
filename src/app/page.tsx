153783c4e4aef43080fe9419890497befde0ca57
AloysJehwin <aloysjehwin@gmail.com>
2025-07-27 23:53:22 +0530

    contact changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -243,7 +243,7 @@ export default function Portfolio() {
                   </div>
                 </a>
                 
-                <a href="https://linkedin.com/in/harini-g" className="group bg-gray-800/50 border border-green-500/30 rounded-lg p-4 hover:bg-green-500/5 hover:border-green-400 transition-all duration-300 transform hover:-translate-y-1">
+                <a href="https://www.linkedin.com/in/harini-g-27b269230/" className="group bg-gray-800/50 border border-green-500/30 rounded-lg p-4 hover:bg-green-500/5 hover:border-green-400 transition-all duration-300 transform hover:-translate-y-1">
                   <div className="flex items-center">
                     <Linkedin className="w-5 h-5 text-green-400 group-hover:text-green-300 mr-3" />
                     <div>
